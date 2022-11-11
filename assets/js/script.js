a501b126bf3c0aa4cd8e9991c947e52ced7d7155
courtney <xoc0urtney14xo@aim.com>
2022-11-10 23:09:15 -0500

    added variables to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var thequizBody = document.getElementById("thequiz");
+var finishedResultsEL = document.getElementById("finishedresult");
+var finishedScoreEl = document.getElementById("finishedscore");
+var quizDoneDiv = document.getElementById("quizdone");
+var questionsEL = document.getElementById("questions");
+var theQuizTime = document.getElementById("time");
+var mainPageContainer = document.getElementById("mainpage");
+var startQuizButton = document.getElementById("start-button");
+var startQuizDiv = document.getElementById("mainpage");
+var scoreContainer = document.getElementById("score-container");
+var scorePageDiv = document.getElementById("scorepage");
+var nameInput = document.getElementById("name");
+var scoreDisplay = document.getElementById("score-name");
+var finishedButtons = document.getElementById("finishedbuttons");
+var enterButton = document.getElementById("enter");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
+var quizQuestions = [{
+    
+}]
